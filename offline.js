ebbd552f72183d54ec64458a6025dee8c9e0abfd
RenatikSun <46327362+RenatikSun@users.noreply.github.com>
2019-04-06 15:56:33 +0300

    SQ

---
+++ offline.js
@@ -0,0 +1,18 @@
+﻿{
+	"version": 1554555251,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/answer1-sheet0.png",
+		"images/answer1-sheet1.png",
+		"images/answer1-sheet2.png",
+		"images/background.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
